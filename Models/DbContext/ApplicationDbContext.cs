03e88f5001ae5ea12ad1dfc3d69e339fac6452ce
Mistaweng <122454766+Mistaweng@users.noreply.github.com>
2023-07-27 01:57:45 +0100

    Initial commit

---
+++ Models/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactBook_API.Models.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
